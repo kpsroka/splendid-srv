fc8e1a7f2162c06997223c9e087eb4a47d8ff407
Krzysztof Sroka <kpsroka@users.noreply.github.com>
2017-07-24 15:45:57 +0200

    Adding new mapping for getting Game State and creating a semi-randomized fake state.

--- src/main/java/net/rk/splendid/GameDataController.java
+++ src/main/java/net/rk/splendid/GameDataController.java
@@ -2,15 +2,21 @@ package net.rk.splendid;
 
 import net.rk.splendid.dao.GameConfig;
 import net.rk.splendid.dao.GameRef;
+import net.rk.splendid.dao.GameState;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/game")
-public final class GameConfigController {
+public final class GameDataController {
   @RequestMapping("/getConfig")
   public GameConfig getGameConfig(@RequestParam("id") String gameRefId) {
     return new GameConfig(new GameRef(gameRefId));
+  }
+
+  @RequestMapping("/getState")
+  public GameState getGameState(@RequestParam("id") String gameRefId) {
+    return new GameState(gameRefId);
   }
 }
--- src/main/java/net/rk/splendid/dao/GameState.java
+++ src/main/java/net/rk/splendid/dao/GameState.java
@@ -5,10 +5,11 @@ public final class GameState {
   private final Board board;
   private final PlayerState[] playerState;
 
-  public GameState(int round, Board board, PlayerState[] playerState) {
-    this.round = round;
-    this.board = board;
-    this.playerState = playerState;
+  public GameState(String gameRef) {
+    this.round = 0;
+    this.board = FakeData.CreateRandomBoard();
+    this.playerState =
+        FakeData.CreateRandomPlayerState(FakeData.FIXED_PLAYERS.length);
   }
 
   public int getRound() {
