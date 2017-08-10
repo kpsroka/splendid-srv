86dad20276510939878f7dc49853f6037408577b
Krzysztof Sroka <kpsroka@users.noreply.github.com>
2017-08-10 16:32:44 +0200

    Updating GameConfig's initialization.

--- src/main/java/net/rk/splendid/dto/GameConfig.java
+++ src/main/java/net/rk/splendid/dto/GameConfig.java
@@ -2,12 +2,18 @@ package net.rk.splendid.dto;
 
 public final class GameConfig {
   private final GameRef gameRef;
+  private final Player[] players;
 
   public GameConfig(GameRef gameRef) {
+    this(gameRef, FakeData.FIXED_PLAYERS);
+  }
+
+  public GameConfig(GameRef gameRef, Player[] players) {
     this.gameRef = gameRef;
+    this.players = players;
   }
 
   public GameRef getRef() { return gameRef; }
 
-  public Player[] getPlayers() { return FakeData.FIXED_PLAYERS; }
+  public Player[] getPlayers() { return players; }
 }
