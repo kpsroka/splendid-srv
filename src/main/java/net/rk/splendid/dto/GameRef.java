f385681c6daaecfe0ae586f795eb249f87277fb1
Krzysztof Sroka <kpsroka@users.noreply.github.com>
2017-08-31 10:41:17 +0200

    Adding playerToken to GameRef DTO.

--- src/main/java/net/rk/splendid/dto/GameRef.java
+++ src/main/java/net/rk/splendid/dto/GameRef.java
@@ -1,13 +1,23 @@
 package net.rk.splendid.dto;
 
 public final class GameRef {
-  private final String id;
+  private final String gameId;
+  private final String playerToken;
 
   public GameRef(String id) {
-    this.id = id;
+    this(id, "unset");
+  }
+
+  public GameRef(String gameId, String playerToken) {
+    this.gameId = gameId;
+    this.playerToken = playerToken;
   }
 
   public String getId() {
-    return this.id;
+    return this.gameId;
+  }
+
+  public String getPlayerToken() {
+    return playerToken;
   }
 }
