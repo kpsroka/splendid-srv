311bae4635eb904b8f122ac11953fbc0df5e573d
Krzysztof Sroka <kpsroka@users.noreply.github.com>
2017-09-06 15:23:27 +0200

    Making PlayerNameEmptyException extend IllegalArgumentException.

--- src/main/java/net/rk/splendid/exceptions/PlayerNameEmptyException.java
+++ src/main/java/net/rk/splendid/exceptions/PlayerNameEmptyException.java
@@ -4,5 +4,5 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(value=HttpStatus.BAD_REQUEST, reason="Player name empty")
-public final class PlayerNameEmptyException extends RuntimeException {
+public final class PlayerNameEmptyException extends IllegalArgumentException {
 }
